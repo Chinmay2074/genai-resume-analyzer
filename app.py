ec0be90cb94546c6fefdaf606dd61dfa597f4aca
Chinmay2074 <74054184+Chinmay2074@users.noreply.github.com>
2026-03-09 00:39:32 +0530

    app.py

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 import streamlit as st
 import PyPDF2
 
-st.title("AI Resume Analyzer")
+st.title("Simple Resume Analyzer")
 
 uploaded_file = st.file_uploader("Upload Resume PDF", type="pdf")
-
 job_description = st.text_area("Paste Job Description")
-
 
 def extract_text(file):
     reader = PyPDF2.PdfReader(file)
@@ -16,9 +14,7 @@ def extract_text(file):
             text += page.extract_text()
     return text
 
-
 if st.button("Analyze Resume"):
-
     if uploaded_file and job_description:
 
         resume_text = extract_text(uploaded_file)
@@ -26,16 +22,13 @@ if st.button("Analyze Resume"):
         resume_words = set(resume_text.lower().split())
         jd_words = set(job_description.lower().split())
 
-        matching = resume_words.intersection(jd_words)
+        matches = resume_words.intersection(jd_words)
 
-        score = int((len(matching) / len(jd_words)) * 100)
+        score = int((len(matches) / len(jd_words)) * 100)
 
-        st.subheader("Analysis Result")
-
+        st.subheader("Result")
         st.write("Match Score:", score, "%")
-
-        st.write("Matching Keywords:")
-        st.write(list(matching)[:10])
+        st.write("Matching words:", list(matches)[:10])
 
     else:
         st.warning("Upload resume and paste job description")
